8df48bb4d7cba5c586d82f4be28b9e678dffbff5
Kuba <jkb.kam@gmail.com>
2018-05-02 22:24:09 +0200

    adding contacts

--- src/ContactsView.js
+++ src/ContactsView.js
@@ -4,13 +4,40 @@ import React, {Component} from 'react'
 class ContactsView extends Component {
     state = {
         contactName: 'janusz',
-        contactNumber: '123123'
+        contactNumber: '123123',
+        contactEmail: 'emloe@melfo',
+        contactCategory: 'family',
+        contacts: [
+            {
+                id: 1,
+                name: 'janek',
+                number: '1212',
+                email: 'eloelo@',
+                category: 'family'
+            },
+            {
+                id: 2,
+                name: 'john',
+                number: '122312',
+                email: 'eloelo@',
+                category: 'family'
+            }
+        ]
     }
     handleSubmit = event => {
         event.preventDefault()
-        console.log(this.state.contactName, this.state.contactNumber)
-    }
+        console.log(this.state.contactName, this.state.contactNumber, this.state.contactEmail, this.state.contactCategory)
 
+        this.setState({
+            contacts: this.state.contacts.concat({
+                id: this.state.contacts.length + 1,
+                name: this.state.contactName,
+                email: this.state.contactEmail,
+                category: this.state.contactCategory
+
+            })
+        })
+    }
     handleChange = event => {
         this.setState({
             contactName: event.target.value
@@ -19,6 +46,18 @@ class ContactsView extends Component {
     handleContactNumberChange = event => {
         this.setState({
             contactNumber: event.target.value
+        })
+    }
+
+    handleContactEmailChange = event => {
+        this.setState({
+            contactEmail: event.target.value
+        })
+    }
+
+    handleContactCategoryChange = event => {
+        this.setState({
+            contactCategory: event.target.value
         })
     }
 
@@ -36,12 +75,30 @@ class ContactsView extends Component {
                         value={this.state.contactNumber}
                         onChange={this.state.handleContactNumberChange}
                         />
+                    <br />
+                    <input
+                        value={this.state.contactEmail}
+                        onChange={this.handleContactEmailChange}
+                    />
+                    <br />
+                    <input
+                        value={this.state.contactCategory}
+                        onChange={this.handleContactCategoryChange}
+                    />
                     <button>Add</button>
                 </form>
 
                 <ul>
-                    <li>First contact</li>
-                    <li>Second contact</li>
+                    {
+                   this.state.contacts.map(
+                    contact => (
+                        <li key={contact.id}>
+                            {contact.name} <br />
+                            {contact.number} : {contact.email} : {contact.category}
+                        </li>
+                    )
+                   )
+                    }
                 </ul>
             </div>
 
